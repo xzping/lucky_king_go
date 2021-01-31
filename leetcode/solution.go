7a7eb851305c3ca48fe9b4c580f9a5f86c8b4a6e
xiezhiping <xiezhiping.96@bytedance.com>
2021-01-31 10:40:49 +0800

    2021-1-31

---
+++ leetcode/solution.go
@@ -0,0 +1,25 @@
+package leetcode
+
+// 暴力遍历
+func twoSum(nums []int, target int) []int {
+	for i, x := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if x+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// hash表
+func twoSum2(nums []int, target int) []int {
+	hashTable := map[int]int{}
+	for i, x := range nums {
+		if p, ok := hashTable[target-x]; ok {
+			return []int{p, i}
+		}
+		hashTable[x] = i
+	}
+	return nil
+}
